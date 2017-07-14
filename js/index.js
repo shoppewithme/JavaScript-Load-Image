253917a90982ac02b85d8da38eedd0cc041424b2
Adam InTae Gerard <Thoughtscript@thoughtscript.io>
2017-07-13 21:35:41 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
-module.exports = require('./load-image')
-
-require('./load-image-scale')
-require('./load-image-meta')
-require('./load-image-fetch')
-require('./load-image-exif')
-require('./load-image-exif-map')
-require('./load-image-orientation')
+module.exports = {
+  load: require('./load-image'),
+  scale: require('./load-image-scale'),
+  meta: require('./load-image-meta'),
+  fetch: require('./load-image-fetch'),
+  exif: require('./load-image-exif'),
+  exifmap: require('./load-image-exif-map'),
+  orientation: require('./load-image-orientation')
+}
